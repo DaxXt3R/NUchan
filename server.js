4b1bfc308fdc47b6cb94831872506c8878650f04
DaXt3R <deennikolov@gmail.com>
2024-11-11 17:52:14 +0200

    setup browser-sync, and node --watch instead of nodemon

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,"views"))
 app.set(express.static(path.join(__dirname,"public"))) //serve public directory
 
+const browserSync = require('browser-sync').create();
+browserSync.init({
+    proxy: "http://localhost:1001",
+    // setup what files or folders should be watched **/* means watch a folder recursively
+    files: ["views/**/*"],
+    port: 3000,
+    open: false
+})
 
-
-
-app.listen(3000, ()=>{
-    console.log("http://localhost:3000/")
+app.listen(1001, ()=>{
+    console.log("http://localhost:1001/")
 })
 
 app.get('/', (req,res)=>{
     res.render('index.ejs')
+    console.log("")
 })
