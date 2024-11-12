6dbb8f59fa3db075d69efa617689487bc4e44e99
DaXt3R <deennikolov@gmail.com>
2024-11-12 19:21:18 +0200

    added tailwind

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app=express();    //create instance of express
 
 const path=require("path")
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,"views"))
-app.set(express.static(path.join(__dirname,"public"))) //serve public directory
+app.set('views', path.join(__dirname,"/views"))
+app.use(express.static(path.join(__dirname,"/public"))) //serve public directory
 
 // --------------------------- BROWSER SYNC ---------------------------
 const browserSync = require('browser-sync').create();
@@ -14,7 +14,7 @@ app.listen(1001, ()=>{
 browserSync.init({
     proxy: "http://localhost:1001", //this 
     // setup what files or folders should be watched **/* means watch a folder recursively
-    files: ["views/**/*"],
+    files: ["views/**/*", "public/scripts"],
     port: 3000, //the port that browserSync uses
     open: false //dont open a new tab
 })
